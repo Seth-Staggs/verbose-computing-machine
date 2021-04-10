637eb9f2fe80c556db47142044583129f1c380fc
Seth <Sethstaggs2@gmail.com>
2021-04-10 12:41:47 -0500

    finished Services

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -13,7 +13,7 @@ const AboutMe = () => {
                 </div>
                 <div className='col-lg-6 col-xm-12'>
                     <h1 className="aboutme-header"> About me:</h1>
-                    <p>Howdy I'm Seth, and I'm a Full-Stack Web Developer. I create responsive websites for phone, tablet, and pc.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet massa vitae tortor condimentum lacinia quis vel. Nulla aliquet porttitor lacus luctus accumsan tortor posuere ac ut. Convallis tellus id interdum velit laoreet id donec. Sed tempus urna et pharetra pharetra. Purus semper eget duis at tellus at urna condimentum mattis. Fames ac turpis egestas integer eget aliquet nibh praesent tristique. Risus at ultrices mi tempus imperdiet nulla malesuada pellentesque. Nulla facilisi cras fermentum odio eu feugiat pretium nibh. Ac turpis egestas integer eget. Libero justo laoreet sit amet. Semper eget duis at tellus. Amet cursus sit amet dictum sit. Aliquam id diam maecenas ultricies mi eget mauris pharetra et. Non quam lacus suspendisse faucibus. Eget mauris pharetra et ultrices neque ornare.</p>
                 </div>
             </div>
         </div>
---
+++ src/components/Services.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faFileCode, faDesktop } from '@fortawesome/free-solid-svg-icons';
+
+const Services = () => {
+    return (
+        <div className='services'>
+            <h1 className='py-5'>My Services</h1>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-md-3 col-sm-6'>
+                        <div className='box'>
+                            <div className='circle'>
+                                <FontAwesomeIcon className='service-icon'icon={faDesktop} size='2x' />
+                            </div>
+                            <h3>Web Design</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris nunc congue nisi vitae.</p>
+                        </div>
+                    </div>
+                    <div className='col-md-3 col-sm-6'>
+                        <div className='box'>
+                            <div className='circle'>
+                                <FontAwesomeIcon className='service-icon'icon={faFileCode} size='2x' />
+                            </div>
+                            <h3>Web Development</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris nunc congue nisi vitae.</p>
+                        </div>
+                    </div>
+                    <div className='col-md-3 col-sm-6'>
+                        <div className='box'>
+                            <div className='circle'>
+                                <FontAwesomeIcon className='service-icon'icon={faFacebookF} size='2x' />
+                            </div>
+                            <h3>SMM</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris nunc congue nisi vitae.</p>
+                        </div>
+                    </div>
+                    <div className='col-md-3 col-sm-6'>
+                        <div className='box'>
+                            <div className='circle'>
+                                <FontAwesomeIcon className='service-icon'icon={faGoogle} size='2x' />
+                            </div>
+                            <h3>Google Ads</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris nunc congue nisi vitae.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Services;
