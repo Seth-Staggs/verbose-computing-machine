2faf466c64fb4839d42d5b00d40399d2b77f2f04
Seth <Sethstaggs2@gmail.com>
2021-04-09 22:22:06 -0500

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+import logo from '../images/logo.png';
+
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#"><img className='logo' src={logo} alt='logo...' /></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <FontAwesomeIcon icon={faBars} style={{ color: "#f8f8ff"}}/>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" aria-current="page" href="#">Home <span className='sr-only'>(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">about me</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">services</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">work process</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">portfolio</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact me</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
