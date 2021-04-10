f17f83b0d5b774aaab8ada75b89547c038ca6fd9
Seth <Sethstaggs2@gmail.com>
2021-04-10 11:22:18 -0500

    About me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,12 +7,12 @@ const AboutMe = () => {
         <div className='container py-5'>
             <div className='row'>
                 <div className='col-lg-6 col-xm-12'>
-                    <div className='photo-wrap'>
+                    <div className='photo-wrap mb-5'>
                         <img className='profile-img' src={Profilepic} alt='Profile'/>
                     </div>
                 </div>
                 <div className='col-lg-6 col-xm-12'>
-                    <h1> About me:</h1>
+                    <h1 className="aboutme-header"> About me:</h1>
                     <p>Howdy I'm Seth, and I'm a Full-Stack Web Developer. I create responsive websites for phone, tablet, and pc.</p>
                 </div>
             </div>
